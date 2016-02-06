7d147be192ada9c3d163b21e4404310247734ccd
Max Soe <maxsoe@gmail.com>
2016-02-06 13:50:04 +0000

    fully working

--- OOP with persistent data/popup.js
+++ OOP with persistent data/popup.js
@@ -10,6 +10,12 @@ var app = {
     var $sku = document.getElementById("sku");
     var $skuInput = document.getElementById("useSku");
 
+    // go get the sku if it's there
+    chrome.runtime.sendMessage({fn: "getSku"}, function(response) {
+      console.log("popup got response", response);
+      $sku.value = response;
+    });
+
     // add a click event on the button
     $skuInput.addEventListener("click", function() {
       console.log("button click", $sku.value);
